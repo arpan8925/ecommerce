13fb242477019a974f843faa88a87b44301214cb
arpan8925 <sbxp1966@gmail.com>
2024-09-13 17:03:59 +0600

    error_fixed

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,6 @@
-python install -r requirements.txt
-python 3.9 manage.py collectstatic
+#!/bin/bash
+# Install the necessary requirements
+pip install -r requirements.txt
+
+# Collect static files without asking for input
+python3.9 manage.py collectstatic --no-input --clear
