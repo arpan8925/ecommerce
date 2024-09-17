f803b6857396dbcf97e67a7aae221a0c9107874a
arpan8925 <sbxp1966@gmail.com>
2024-09-18 00:05:20 +0600

    Models Created

--- core/models.py
+++ core/models.py
@@ -3,6 +3,30 @@ from shortuuid.django_fields import ShortUUIDField
 from django.utils.html import mark_safe
 from userauths.models import User
 
+STATUS_CHOICE = (
+    ("process", "Processing"),
+    ("shipped", "Shipped"),
+    ("delivered", "Delivered"),
+)
+
+STATUS = (
+    ("draft", "Draft"),
+    ("disabled", "Disabled"),
+    ("rejected", "Rejected"),
+    ("in_review", "In Review"),
+    ("published", "Published"),
+)
+
+RATING = (
+    ("1", "★☆☆☆☆"),
+    ("2", "★★☆☆☆"),
+    ("3", "★★★☆☆"),
+    ("4", "★★★★☆"),
+    ("5", "★★★★★"),
+)
+
+
+
 
 def user_directory_path(instance, filename):
     return 'user_{0}/{1}'.format(instance.user.id, filename)
@@ -21,6 +45,9 @@ class Category(models.Model):
     def __str__(self):
         return self.title
     
+class Tags(models.Model):
+    pass
+    
 class Vendor(models.Model):
     vid = ShortUUIDField(unique=True, length=10, max_length=20, prefix="ven", alphabet="abcde12345")
     title = models.CharField(max_length=100)
@@ -34,4 +61,126 @@ class Vendor(models.Model):
     days_return = models.CharField(max_length=100)
     warrenty_period = models.CharField(max_length=100)
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        verbose_name_plural = "Vendors"
+
+    def vendor_image(self):
+        return mark_safe('<img src="%s" width="50" height="50" />' % (self.image.url))
     
+    def __str__(self):
+        return self.title
+    
+
+
+class Product(models.Model):
+    pid = ShortUUIDField(unique=True, length=10, max_length=20, prefix="ven", alphabet="abcde12345")
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    title = models.CharField(max_length=100, default="Fresh Pear")
+    image = models.ImageField(upload_to=user_directory_path, default="product.jpg")
+    description = models.TextField(null=True, blank=True, default="This is the product")
+    price = models.DecimalField(max_digits=9999999999999, decimal_places=2, default="1.99")
+    old_price = models.DecimalField(max_digits=9999999999999, decimal_places=2, default="2.99")
+    specification = models.TextField(null=True, blank=True)
+    tags = models.ForeignKey(Tags, on_delete=models.SET_NULL, null=True)
+    product_status = models.CharField(choices=STATUS, max_length=10, default="in_review")
+    status = models.BooleanField(default=True)
+    in_stock = models.BooleanField(default=True)
+    featured = models.BooleanField(default=True)
+    digital = models.BooleanField(default=True)
+    sku = ShortUUIDField(unique=True, length=4, max_length=10, prefix="sku", alphabet="1234567890")
+    date = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(null=True, blank=True)
+    class Meta:
+        verbose_name_plural = "Products"
+    def product_image(self):
+        return mark_safe('<img src="%s" width="50" height="50" />' % (self.image.url))   
+    def __str__(self):
+        return self.title
+    
+    def get_percentage(self):
+        new_price = (self.price/self.old_price) * 100
+        return new_price
+    
+class ProductImages(models.Model):
+    image = models.ImageField(upload_to="product_image", default="product.jpg")
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = "Product Images"
+
+
+######################### Cart - Order - OrderItems - Address ##########################
+
+
+
+
+class CartOrder(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=9999999999999, decimal_places=2, default="1.99")
+    paid_status = models.BooleanField(default=False)
+    order_date = models.DateTimeField(auto_now_add=True)
+    product_status = models.CharField(choices=STATUS_CHOICE, max_length=30, default="process")
+
+    class Meta:
+        verbose_name_plural = "Cart Order"
+
+
+
+class CartOrderItems(models.Model):
+    order = models.ForeignKey(CartOrder, on_delete=models.CASCADE)
+    product_status = models.CharField(max_length=200)
+    item = models.CharField(max_length=200)
+    image = models.CharField(max_length=200)
+    qty = models.IntegerField(default=0)
+    price = models.DecimalField(max_digits=9999999999999, decimal_places=2, default="1.99")
+    total = models.DecimalField(max_digits=9999999999999, decimal_places=2, default="1.99")
+
+    class Meta:
+        verbose_name_plural = "Cart Order Items"
+
+    def order_image(self):
+        return mark_safe('<img src="/media/%s" width="50" height="50" />' % (self.image))
+    
+
+######################### Product Review - Wishlist - Address ##########################
+
+class ProductReview(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    review = models.TextField()
+    rating = models.IntegerField(choices=RATING, default=None)
+    date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = "Product Reviews"
+
+    def __str__(self):
+        return self.product.title
+    
+    def get_rating(self):
+        return self.rating
+    
+
+class Wishlist(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = "Product Reviews"
+
+    def __str__(self):
+        return self.product.title
+    
+class Address(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    address = models.CharField(max_length=100, null=True)
+    status = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name_plural = "Address"
+
+
