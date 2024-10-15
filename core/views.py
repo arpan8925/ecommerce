d6860511bdf047cce5db50b2b205a7a68f1f23ee
arpan8925 <sbxp1966@gmail.com>
2024-10-15 23:01:47 +0600

    Product View In Homepage First Column

--- core/views.py
+++ core/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from core.models import Product, Category, Vendor, CartOrder, CartOrderItems, ProductImages, ProductReview, Wishlist, Address
 
 # Create your views here.
 
 def index(request):
+    # product = Product.objects.all().order_by("-id")
 
-    return render(request, 'core/index.html')
+    product = Product.objects.filter(product_status = "published", featured = True).order_by("-id")
+
+    context = {
+        "products" : product
+    }
+    return render(request, 'core/index.html', context)
